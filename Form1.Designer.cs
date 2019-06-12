e6fd07eb8f634c68313907d0644e7fcb5076aa4b
Adam Chlebek <adamchlebek@live.com>
2019-06-12 10:07:23 -0400

    Updated Calls and UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,41 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSingleImage = new System.Windows.Forms.Button();
-            this.btnMultipleImages = new System.Windows.Forms.Button();
             this.btnDownload = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.lblStatus = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.lblPixelCount = new System.Windows.Forms.Label();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSingleImage = new System.Windows.Forms.Button();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.lblTotalPixelCount = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // btnSingleImage
-            // 
-            this.btnSingleImage.Location = new System.Drawing.Point(13, 13);
-            this.btnSingleImage.Name = "btnSingleImage";
-            this.btnSingleImage.Size = new System.Drawing.Size(775, 67);
-            this.btnSingleImage.TabIndex = 0;
-            this.btnSingleImage.Text = "Upload Single Image";
-            this.btnSingleImage.UseVisualStyleBackColor = true;
-            this.btnSingleImage.Click += new System.EventHandler(this.BtnSingleImage_Click);
-            // 
-            // btnMultipleImages
-            // 
-            this.btnMultipleImages.Location = new System.Drawing.Point(12, 86);
-            this.btnMultipleImages.Name = "btnMultipleImages";
-            this.btnMultipleImages.Size = new System.Drawing.Size(775, 67);
-            this.btnMultipleImages.TabIndex = 1;
-            this.btnMultipleImages.Text = "Upload Multiple Image";
-            this.btnMultipleImages.UseVisualStyleBackColor = true;
             // 
             // btnDownload
             // 
             this.btnDownload.Enabled = false;
-            this.btnDownload.Location = new System.Drawing.Point(13, 343);
+            this.btnDownload.Location = new System.Drawing.Point(17, 422);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(4);
             this.btnDownload.Name = "btnDownload";
-            this.btnDownload.Size = new System.Drawing.Size(774, 95);
+            this.btnDownload.Size = new System.Drawing.Size(1032, 117);
             this.btnDownload.TabIndex = 2;
             this.btnDownload.Text = "Download";
             this.btnDownload.UseVisualStyleBackColor = true;
@@ -75,56 +58,94 @@
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(375, 249);
+            this.lblStatus.Location = new System.Drawing.Point(500, 306);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(47, 13);
+            this.lblStatus.Size = new System.Drawing.Size(61, 17);
             this.lblStatus.TabIndex = 3;
             this.lblStatus.Text = "Ready...";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(13, 223);
+            this.progressBar1.Location = new System.Drawing.Point(17, 274);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(774, 23);
+            this.progressBar1.Size = new System.Drawing.Size(1032, 28);
             this.progressBar1.TabIndex = 4;
             // 
             // lblPixelCount
             // 
             this.lblPixelCount.AutoSize = true;
-            this.lblPixelCount.Location = new System.Drawing.Point(12, 327);
+            this.lblPixelCount.Location = new System.Drawing.Point(16, 402);
+            this.lblPixelCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPixelCount.Name = "lblPixelCount";
-            this.lblPixelCount.Size = new System.Drawing.Size(66, 13);
+            this.lblPixelCount.Size = new System.Drawing.Size(144, 17);
             this.lblPixelCount.TabIndex = 5;
-            this.lblPixelCount.Text = "Pixel Count: ";
+            this.lblPixelCount.Text = "Different Pixel Count: ";
+            // 
+            // btnSingleImage
+            // 
+            this.btnSingleImage.Location = new System.Drawing.Point(17, 16);
+            this.btnSingleImage.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSingleImage.Name = "btnSingleImage";
+            this.btnSingleImage.Size = new System.Drawing.Size(1033, 82);
+            this.btnSingleImage.TabIndex = 0;
+            this.btnSingleImage.Text = "Upload Single Image";
+            this.btnSingleImage.UseVisualStyleBackColor = true;
+            this.btnSingleImage.Click += new System.EventHandler(this.BtnSingleImage_Click);
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(17, 106);
+            this.btnFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(1033, 82);
+            this.btnFolder.TabIndex = 6;
+            this.btnFolder.Text = "Upload Folder";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // lblTotalPixelCount
+            // 
+            this.lblTotalPixelCount.AutoSize = true;
+            this.lblTotalPixelCount.Location = new System.Drawing.Point(16, 381);
+            this.lblTotalPixelCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalPixelCount.Name = "lblTotalPixelCount";
+            this.lblTotalPixelCount.Size = new System.Drawing.Size(86, 17);
+            this.lblTotalPixelCount.TabIndex = 7;
+            this.lblTotalPixelCount.Text = "Pixel Count: ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblTotalPixelCount);
+            this.Controls.Add(this.btnFolder);
             this.Controls.Add(this.lblPixelCount);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnDownload);
-            this.Controls.Add(this.btnMultipleImages);
             this.Controls.Add(this.btnSingleImage);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnSingleImage;
-        private System.Windows.Forms.Button btnMultipleImages;
         private System.Windows.Forms.Button btnDownload;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label lblPixelCount;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnSingleImage;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Label lblTotalPixelCount;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,8 @@ namespace ColorPaletteGenerator
 {
     public partial class Form1 : Form
     {
+        private string folderPath;
+
         List<Color> pixelList;
         public Form1()
         {
@@ -30,6 +32,8 @@ namespace ColorPaletteGenerator
             dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
             if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
             {
+                folderPath = Path.GetDirectoryName(dialog.FileName);
+
                 var t = new Thread(() => pixelRead(dialog));
                 t.Start();
                 lblStatus.Text = "Loading...";
@@ -40,8 +44,11 @@ namespace ColorPaletteGenerator
             pixelList = new List<Color>();
 
             Bitmap img = new Bitmap(dialog.FileName);
+            int pixelCount = 0;
 
             progressBar1.Maximum = img.Width;
+
+            pixelCount += img.Width + img.Height;
 
             for (int i = 0; i < img.Width; i++)
             {
@@ -60,23 +67,24 @@ namespace ColorPaletteGenerator
 
             progressBar1.Value = progressBar1.Maximum;
 
+            lblTotalPixelCount.Text = $"Pixel Count: {pixelCount}";
             lblPixelCount.Text = $"Pixel Count: {pixelList.Count}";
             lblStatus.Text = "Done";
 
             btnDownload.Enabled = true;
+            btnSingleImage.Enabled = false;
+            btnFolder.Enabled = false;
         }
 
         private void BtnDownload_Click(object sender, EventArgs e)
         {
-            IronPdf.HtmlToPdf render = new IronPdf.HtmlToPdf();
-
             string htmlString = "<div style=\"padding: 5px;\">";
 
             int count = 0;
 
             foreach (Color color in pixelList) {
                 count += 1;
-                if (count < 20)
+                if (count < 280)
                 {
                     htmlString += "<li style=\"float: left; \">";
                     htmlString += $"<div style=\"display: flex; margin: 3px; width: 50px; height: 50px; background: rgba({color.R}, {color.G}, {color.B}, {color.A}) ;\"></div>";
@@ -95,13 +103,104 @@ namespace ColorPaletteGenerator
             htmlString += "</div>";
 
             lblStatus.Text = "Rendering PDF...";
+            Application.DoEvents();
 
-            render.RenderHtmlAsPdf(htmlString).SaveAs(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/palette.pdf");
+            RenderPdfAsync(htmlString);
+        }
 
+        private void RenderPdfAsync(string htmlString)
+        {
+            IronPdf.HtmlToPdf render = new IronPdf.HtmlToPdf();
+
+            
             lblStatus.Text = "Saved!";
 
-            MessageBox.Show("File Saved to Desktop");
+            render.RenderHtmlAsPdf(htmlString).SaveAs(folderPath + "/palette.pdf");
 
+            MessageBox.Show("File Saved to " + folderPath);
+
+            pixelList = new List<Color>();
+            btnDownload.Enabled = false;
+            btnSingleImage.Enabled = true;
+            btnFolder.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            List<Color> pixelList = new List<Color>();
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    var t = new Thread(() => pixelReadFolder(fbd));
+                    t.Start();
+                    lblStatus.Text = "Loading...";
+                }
+            }
+        }
+
+        public void pixelReadFolder(FolderBrowserDialog result)
+        {
+            folderPath = Path.GetFullPath(result.SelectedPath);
+
+            pixelList = new List<Color>();
+            int pixelCount = 0;
+
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".jpe", ".jfif", ".png", ".PNG" };
+
+            var files = Directory
+                .GetFiles(result.SelectedPath)
+                .Where(file => allowedExtensions.Any(file.ToLower().EndsWith))
+                .ToList();
+
+            progressBar1.Maximum = 0;
+
+            foreach (var file in files)
+            {
+                Bitmap img = new Bitmap(file);
+
+                progressBar1.Maximum += img.Width;
+            }
+
+            foreach (var file in files)
+            {
+                Bitmap img = new Bitmap(file);
+
+                pixelCount += img.Width + img.Height;
+
+                for (int i = 0; i < img.Width; i++)
+                {
+                    for (int j = 0; j < img.Height; j++)
+                    {
+                        Color pixel = img.GetPixel(i, j);
+
+                        if (!pixelList.Contains(pixel))
+                        {
+                            pixelList.Add(pixel);
+                        }
+                    }
+
+                    progressBar1.Value += 1;
+                }
+            }
+
+            progressBar1.Value = progressBar1.Maximum;
+            
+            lblTotalPixelCount.Text = $"Pixel Count: {pixelCount}";
+            lblPixelCount.Text = $"Different Pixel Count: {pixelList.Count}";
+            lblStatus.Text = "Done";
+            
+            btnDownload.Enabled = true;
+            btnSingleImage.Enabled = false;
+            btnFolder.Enabled = false;
         }
     }
 }
